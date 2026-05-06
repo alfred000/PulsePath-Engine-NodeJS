d02a4a69362670b1925b303cf8c6e7904ba3b925
Alfred Nzigiye <alfred.nzigiye@gmail.com>
2026-05-06 13:23:08 +0200

    Initial commit

---
+++ src/models/DailyLog.js
@@ -0,0 +1,11 @@
+class DailyLog {
+    constructor(date, weight, caloriesIn, steps, sleepHours) {
+        this.date = new Date(date);
+        this.weight = weight;
+        this.caloriesIn = caloriesIn;
+        this.steps = steps;
+        this.sleepHours = sleepHours;
+    }
+}
+
+module.exports = DailyLog;
